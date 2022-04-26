9f1b1e8cd2a0f14753e464fc8ec6ba81f9424092
bangkit16 <bangkitcaniago@gmail.com>
2022-04-26 23:36:40 +0700

    minggu10

---
+++ homeCRUD.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <h1>DATA MAHASISWA</h1>
+    <button><a style="text-decoration: none;color: black;" href="insertForm.html">Tambah Data</a></button><br><br>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Foto</th>
+            <th>Aksi</th>
+        </tr>
+        <?php
+
+        include "myconnection.php";
+
+        $query = "SELECT * FROM student";
+        $result = mysqli_query($connect, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+
+        ?>
+                <tr>
+                    <td><?php echo $row["id"]; ?></td>
+                    <td><?php echo $row["name"]; ?></td>
+                    <td><?php echo $row["address"]; ?></td>
+                    <td><img width="200" src="img/<?php echo $row["foto"]; ?>"></td>
+                    <td>
+                        <a href="editForm.php?id=<?php echo $row["id"]; ?>"><button>Edit</button></a>
+                        <a href="delete.php?id=<?php echo $row["id"]; ?>"><button>Hapus</button></a>
+                    </td>
+                </tr>
+        <?php
+            }
+        } else {
+            echo "0 results";
+        }
+        ?>
+    </table>
+</body>
+
+</html>
